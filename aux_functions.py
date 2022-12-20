1fb813f8739113775c3a6788b11b609f6c8cf6db
vincent05r <111105725+vincent05r@users.noreply.github.com>
2022-12-20 22:53:05 +1100

    temp update

--- aux_functions.py
+++ aux_functions.py
@@ -3,6 +3,16 @@ import tensorflow as tf
 
 class aux_functions():
 
+    def scheduler_1(epoch, lr):
+        if epoch < 8:
+            return lr
+        elif epoch < 40:
+            return lr * tf.math.exp(-0.05)
+        else:
+            return lr
+
+
+
     #inspect time series window
     def window_inspection(t_window, mode, label=['open']):
         if mode == True:
@@ -19,19 +29,25 @@ class aux_functions():
     
 
     #compile and fit using tensorflow data window
-    def compile_and_fit(model, window, epochs=20, es_patience=4, es_monitor='val_loss', es_mode = 'min'):
+    def compile_and_fit(model, window, epochs=20, es_patience=4, es_monitor='val_loss', es_mode = 'min', lr = 0.001):
         early_stopping = tf.keras.callbacks.EarlyStopping(monitor = es_monitor,
                                                         patience = es_patience,
                                                         mode = es_mode)
 
+        optimizer_adam = tf.keras.optimizers.Adam(learning_rate=lr)
+
         model.compile(loss=tf.keras.losses.MeanSquaredError(),
-                    optimizer=tf.keras.optimizers.Adam(),
+                    optimizer=optimizer_adam,
                     metrics=[tf.keras.metrics.MeanAbsolutePercentageError()])
+
+        multistep_lr = tf.keras.callbacks.LearningRateScheduler(aux_functions.scheduler_1)
 
         history = model.fit(window.train, epochs=epochs,
                         validation_data=window.val,
-                        callbacks=[early_stopping])
+                        callbacks=[early_stopping, multistep_lr])
         
+        print("Learning rate of the model: ", optimizer_adam._decayed_lr('float32').numpy())
+
         model.summary()
         
         return history
